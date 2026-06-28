5ee64679181e5a920528eae464adf408ee92967e
Sibbala <narasimha1719@gmail.com>
2026-06-28 16:29:41 +0530

    all liniux git commands

---
+++ shell linux .sh
@@ -0,0 +1,41 @@
+  git init                                                                                                                      
+   3 git add .                                                                                                                     
+   4 git commit -m "creating cmnds file with basic shell commands"                                                                 
+   5 git branch -M main                                                                                                            
+   6 git remote add origin https://github.com/adarshtrainer/project1.git                                                           
+   7 git push -u origin main                                                                                                       
+   8 git add .                                                                                                                     
+   9 git commit -m "Multiple file and folder adding"                                                                               
+  10 git push                                                                                                                      
+  11 git add .                                                                                                                     
+  12 git commit -m "password file from java"                                                                                       
+  13 git push                                                                                                                      
+  14 cls                                                                                                                           
+  15 git status                                                                                                                    
+  16 git status                                                                                                                    
+  17 git status                                                                                                                    
+  18 cld                                                                                                                           
+  19 cls                                                                                                                           
+  20 git branch                                                                                                                    
+  21 git branch yashwanth                                                                                                          
+  22 git branch narasimha                                                                                                          
+  23 git branch                                                                                                                    
+  24 git branch dev                                                                                                                
+  25 git branch                                                                                                                    
+  26 git checkout dev                                                                                                              
+  27 git branch                                                                                                                    
+  28 git checkout main                                                                                                             
+  29 git branch                                                                                                                    
+  30 git checkout dev                                                                                                              
+  31 git branch                                                                                                                    
+  32 cls
+33 git add .                                                                                                                     
+  34 git commit -m "docker chnages:...                                                                                             
+  35 git push                                                                                                                      
+  36 git push --set-upstream origin dev                                                                                            
+  37 git checkout main                                                                                                             
+  38 cls                                                                                                                           
+  39 git checkout dev                                                                                                              
+  40 git checkout main                                                                                                             
+  41 git branch                                                                                                                    
+  42 git pull
